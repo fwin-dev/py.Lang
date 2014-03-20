a627d4c07eb2ff8b1d8c8832bb293aa7c12fed91
Jesse Cowles <jcowles@indigital.net>
2014-03-20 09:24:07 -0400

    moved static descriptors in `FuncTools` to `Disk._Funcs`

--- src/Lang/FuncTools/Abstraction.py
+++ src/Lang/FuncTools/Abstraction.py
@@ -7,25 +7,3 @@ def abstractmethod(method):
 		raise NotImplementedError("Abstract method " + repr(method) + " not implemented")
 	default_abstract_method.__name__ = method.__name__
 	return default_abstract_method
-
-class FuncDesc:
-	LOCAL = 2
-	REMOTE = 3
-
-class ArgDesc:
-	SINGLE = 0
-	MULTI = 1
-
-class Descriptor:
-	@classmethod
-	@abstractmethod
-	def isLocal(cls, funcName):
-		pass
-	@classmethod
-	@abstractmethod
-	def getArgTypes(cls, funcName):
-		pass
-	@classmethod
-	@abstractmethod
-	def getBuiltinFunction(cls, funcName, asStr=False):
-		pass
