74fcdf95203fc1479f1f99dd3b1affb89c4fec29
Jesse Cowles <jcowles@indigital.net>
2014-03-21 16:47:25 -0400

    Singleton, Multiton: now returning OrderedSet for _SingletonMultitonAbstract.getAllClasses

--- src/Lang/ClassTools/Patterns/_singleton_multiton_abstract.py
+++ src/Lang/ClassTools/Patterns/_singleton_multiton_abstract.py
@@ -1,6 +1,8 @@
+from Lang.Struct import OrderedSet
+
 class _SingletonMultitonAbstract(type):
 	def getAllClasses(cls):
-		return [class_ for class_ in cls._instances.iterkeys() if issubclass(class_, cls)]
+		return OrderedSet(class_ for class_ in cls._instances.iterkeys() if issubclass(class_, cls))
 	def getAllInstances(cls):
 		return dict((class_, instance) for class_, instance in cls._instances.iteritems() if issubclass(class_, cls))
 
