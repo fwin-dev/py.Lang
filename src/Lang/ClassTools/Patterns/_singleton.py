ae29c1e3f4e14b332f6649ea830cd63735efd3a3
Jesse Cowles <jcowles@indigital.net>
2014-08-22 16:43:57 -0400

    ClassTools.Patterns.Singleton: Removed singleton classes. Now only using metaclasses. This is because the classes only provided a slight ease of use improvement. Also, when the user wanted to use 2 different metaclasses in 2 different base classes, they could not:
    ```
    TypeError: Error when calling the metaclass bases
      metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the metaclasses of all its bases
    ```

--- src/Lang/ClassTools/Patterns/_singleton.py
+++ src/Lang/ClassTools/Patterns/_singleton.py
@@ -18,67 +18,18 @@ class _Singleton_Abstract(_Meta_SingletonMultitonAbstract):
 			return getattr(instance, name)
 		return obj
 
-class _Meta_Singleton_OnDupRaiseException(_Singleton_Abstract):
+
+class Meta_Singleton_OnDupRaiseException(_Singleton_Abstract):
 	def __call__(cls, *args, **kwargs):
 		if cls in cls._instances.iterkeys():
 			raise DuplicateInstanceException("This class is a singleton, but you tried to create more than one instance")
-		newInstance = super(_Meta_Singleton_OnDupRaiseException, cls).__call__(*args, **kwargs)
+		newInstance = super(Meta_Singleton_OnDupRaiseException, cls).__call__(*args, **kwargs)
 		cls._instances[cls] = newInstance
 		return newInstance
 
-class _Meta_Singleton_OnDupReturnExisting(_Singleton_Abstract):
+class Meta_Singleton_OnDupReturnExisting(_Singleton_Abstract):
 	def __call__(cls, *args, **kwargs):
 		if cls not in cls._instances:
-			newInstance = super(_Meta_Singleton_OnDupReturnExisting, cls).__call__(*args, **kwargs)
+			newInstance = super(Meta_Singleton_OnDupReturnExisting, cls).__call__(*args, **kwargs)
 			cls._instances[cls] = newInstance
 		return cls._instances[cls]
-
-class Singleton_OnDupRaiseException(object):
-	"""
-	A singleton allows only one instance per class. Upon trying to create a second instance of a class, an exception is raised.
-	
-	This class should be the first in your MRO heirarchy in order to have a singleton. Any method calls
-	(including `__init__`) will be applied to only one instance of the class.
-	
-	This class (actually the metaclass behind it) also maintains a dictionary of all created classes and the associated instance,
-	thereby also providing a "class registration" mechanism. To see which classes have been registered:
-	
-		class Foo(Singleton_OnDupRaiseException):
-			def __init__(self):
-				super(Foo, self).__init__()
-				...
-		Foo()
-		Foo.getAllClasses()
-		# [<class '__main__.Foo'>]
-		Foo.getAllInstances()
-		# {<class '__main__.Foo'>: <__main__.Foo object at 0x1062a9210>}
-	
-	http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
-	http://stackoverflow.com/questions/392160/what-are-your-concrete-use-cases-for-metaclasses-in-python
-	"""
-	__metaclass__ = _Meta_Singleton_OnDupRaiseException
-
-class Singleton_OnDupReturnExisting(object):
-	"""
-	A singleton allows only one instance per class. Upon trying to create a second instance of a class, the existing instance is returned.
-	
-	This class should be the first in your MRO heirarchy in order to have a singleton. Any method calls
-	(including `__init__`) will be applied to only one instance of the class.
-	
-	This class (actually the metaclass behind it) also maintains a dictionary of all created classes and the associated instance,
-	thereby also providing a "class registration" mechanism. To see which classes have been registered:
-	
-		class Foo(Singleton_OnDupReturnExisting):
-			def __init__(self):
-				super(Foo, self).__init__()
-				...
-		Foo()
-		Foo.getAllClasses()
-		# [<class '__main__.Foo'>]
-		Foo.getAllInstances()
-		# {<class '__main__.Foo'>: <__main__.Foo object at 0x1062a9210>}
-	
-	http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
-	http://stackoverflow.com/questions/392160/what-are-your-concrete-use-cases-for-metaclasses-in-python
-	"""
-	__metaclass__ = _Meta_Singleton_OnDupReturnExisting
