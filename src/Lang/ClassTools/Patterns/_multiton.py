2e44ef679e9c5b22a2b04b35deebdde8c18d1aa5
Jesse Cowles <jcowles@indigital.net>
2014-08-22 16:53:20 -0400

    ClassTools.Patterns.Multiton: Removed multiton classes. Now only using metaclasses. This is because the classes only provided a slight ease of use improvement. Also, when the user wanted to use 2 different metaclasses in 2 different base classes, they could not:
    ```
    TypeError: Error when calling the metaclass bases
      metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the metaclasses of all its bases
    ```

--- src/Lang/ClassTools/Patterns/_multiton.py
+++ src/Lang/ClassTools/Patterns/_multiton.py
@@ -3,15 +3,15 @@ from _singleton_multiton_abstract import _Meta_SingletonMultitonAbstract, Duplic
 from Lang.Struct import weakref
 from abc import ABCMeta, abstractmethod
 
-class _Meta_Multiton_OneEquivalentInstance(_Meta_SingletonMultitonAbstract, ABCMeta):
+class _Meta_Multiton_eq(_Meta_SingletonMultitonAbstract, ABCMeta):
 	_instances = {}
 	
 	def getAllClasses(cls):
 		cls._multiton_pruneClasses()
-		return super(_Meta_Multiton_OneEquivalentInstance, cls).getAllClasses()
+		return super(_Meta_Multiton_eq, cls).getAllClasses()
 	def getAllInstances(cls):
 		cls._multiton_pruneClasses()
-		return super(_Meta_Multiton_OneEquivalentInstance, cls).getAllInstances()
+		return super(_Meta_Multiton_eq, cls).getAllInstances()
 	
 	@abstractmethod
 	def _multiton_onDup(self, existingInstance, newInstance):
@@ -26,7 +26,7 @@ class _Meta_Multiton_OneEquivalentInstance(_Meta_SingletonMultitonAbstract, ABCM
 		cls._instances = dict((class_, instanceSet) for class_, instanceSet in cls._instances.iteritems() if len(instanceSet) > 0)
 	
 	def __call__(cls, *args, **kwargs):
-		newInstance = super(_Meta_Multiton_OneEquivalentInstance, cls).__call__(*args, **kwargs)		# create new, normal class instance
+		newInstance = super(_Meta_Multiton_eq, cls).__call__(*args, **kwargs)		# create new, normal class instance
 		if cls not in cls._instances:
 			cls._instances[cls] = weakref.WeakSet()
 		for instance in cls._instances[cls]:
@@ -35,44 +35,31 @@ class _Meta_Multiton_OneEquivalentInstance(_Meta_SingletonMultitonAbstract, ABCM
 		cls._instances[cls].add(newInstance)
 		return newInstance
 
-class _Meta_Multiton_OneEquivalentInstance_OnDupRaiseException(_Meta_Multiton_OneEquivalentInstance):
-	def _multiton_onDup(self, existingInstance, newInstance):
-		raise DuplicateInstanceException("An equivalent instance has already been created")
-
-class _Meta_Multiton_OneEquivalentInstance_OnDupReturnExisting(_Meta_Multiton_OneEquivalentInstance):
-	def _multiton_onDup(self, existingInstance, newInstance):
-		return existingInstance
-
-class Multiton_OneEquivalentInstance_OnDupRaiseException(object):
+class Meta_Multiton_eq_OnDupRaiseException(_Meta_Multiton_eq):
 	"""
 	Only one instance of this class is allowed per equivalent instance. An equivalent instance is defined by using the `==` operator.
-	This class should be the first in your MRO heirarchy in order to have a multiton with only one equivalent instance allowed.
 	
 	Because this multiton must use `==` (which can use `__eq__` in the class instance, for example), it must create the instance to do
-	the comparison, which means there will actually be two instances for a short-lived time, while the comparison is being done.
+	the comparison, which means there will actually be two instances for a short-lived time while the comparison is being done.
 	Because of this, since more than one equivalent instance is possible during `__init__` and `__eq__` calls, make sure that those
 	methods do not rely upon there only being a single equivalent instance.
 	
 	If an equivalent instance already exists, an exception will be raised.
 	
 	Similar solutions and useful links:
-	- http://lifegoo.pluskid.org/?p=345
+	@see:	http://lifegoo.pluskid.org/?p=345
 	"""
-	__metaclass__ = _Meta_Multiton_OneEquivalentInstance_OnDupRaiseException
+	
+	def _multiton_onDup(self, existingInstance, newInstance):
+		raise DuplicateInstanceException("An equivalent instance has already been created")
 
-class Multiton_OneEquivalentInstance_OnDupReturnExisting(object):
+class Meta_Multiton_eq_OnDupReturnExisting(_Meta_Multiton_eq):
 	"""
-	Only one instance of this class is allowed per equivalent instance. An equivalent instance is defined by using the `==` operator.
-	This class should be the first in your MRO heirarchy in order to have a multiton with only one equivalent instance allowed.
+	This metaclass is very similar to Meta_Multiton_eq_OnDupRaiseException, but with one difference. Upon trying to
+	create a second equivalent instance to one already made, no exception is raised; the existing equivalent instance (which is kept track
+	of as an internal reference in the metaclass) is returned instead.
 	
-	Because this multiton must use `==` (which can use `__eq__` in the class instance, for example), it must create the instance to do
-	the comparison, which means there will actually be two instances for a short-lived time, while the comparison is being done.
-	Because of this, since more than one equivalent instance is possible during `__init__` and `__eq__` calls, make sure that those
-	methods do not rely upon there only being a single equivalent instance.
-	
-	If an equivalent instance already exists, that instance will be returned instead of creating another one.
-	
-	Similar solutions and useful links:
-	- http://lifegoo.pluskid.org/?p=345
+	@see:	Meta_Multiton_eq_OnDupRaiseException
 	"""
-	__metaclass__ = _Meta_Multiton_OneEquivalentInstance_OnDupReturnExisting
+	def _multiton_onDup(self, existingInstance, newInstance):
+		return existingInstance
